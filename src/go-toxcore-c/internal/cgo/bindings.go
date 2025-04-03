e5f6a33fad66481a73095e1cf3f913fdb0265258
Cam <cammclain@protonmail.com>
2025-04-03 10:30:15 -0500

    Add cgo bindings for the toxcore library

---
+++ src/go-toxcore-c/internal/cgo/bindings.go
@@ -0,0 +1,5 @@
+package cgo
+
+// this file contains the cgo bindings for the toxcore library
+// Toxcore is a C library, so we need to use cgo to call it
+// toxcore can be found at https://github.com/TokTok/c-toxcore
