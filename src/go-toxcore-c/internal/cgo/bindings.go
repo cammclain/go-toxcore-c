e366bc94aa0f14013696e78d84a89d833dd5def1
Cam <cammclain@protonmail.com>
2025-04-03 10:59:51 -0500

    Create dedicated package for low-level cgo bindings

--- src/go-toxcore-c/internal/cgo/bindings.go
+++ src/go-toxcore-c/internal/cgo/bindings.go
@@ -3,3 +3,5 @@ package cgo
 // this file contains the cgo bindings for the toxcore library
 // Toxcore is a C library, so we need to use cgo to call it
 // toxcore can be found at https://github.com/TokTok/c-toxcore
+
+//• Low-Level Bindings: Create a dedicated package (e.g., internal/cgo) that contains all cgo directives and bindings.
