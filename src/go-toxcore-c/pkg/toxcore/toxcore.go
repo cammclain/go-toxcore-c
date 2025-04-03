a3c76fdec5b4c724c3d4f16a435014ed69f914ed
Cam <cammclain@protonmail.com>
2025-04-03 11:03:36 -0500

    Add comment outlining the development of a high-level API to abstract cgo complexity

--- src/go-toxcore-c/pkg/toxcore/toxcore.go
+++ src/go-toxcore-c/pkg/toxcore/toxcore.go
@@ -2,3 +2,5 @@ package toxcore
 
 // Toxcore is a package that provides access to the Toxcore library.
 // It allows you to create and manage Tox instances, send messages, and handle events.
+
+// • High-Level API: In a separate package (e.g., internal/api or in pkg/toxcore), build an idiomatic Go interface that hides the cgo complexity.
